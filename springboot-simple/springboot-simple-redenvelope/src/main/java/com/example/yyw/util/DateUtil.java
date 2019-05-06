952a2b51a71b807373c93e644b0df7acac135b78
yanzhitao <yanzhitao@xiaomalixing.com>
2019-05-06 20:30:32 +0800

    update:日期工具类

--- springboot-simple/springboot-simple-redenvelope/src/main/java/com/example/yyw/util/DateUtil.java
+++ springboot-simple/springboot-simple-redenvelope/src/main/java/com/example/yyw/util/DateUtil.java
@@ -29,7 +29,44 @@ public class DateUtil {
         return localDateTime.format(dateTimeFormatter);
     }
 
-
     public static void main(String[] args) {
+        Date date = new Date();
+        int month = getMonth(date);
+        System.out.println(month);
+        int year = getYear(date);
+        System.out.println(year);
+
+        Date firstDayOfMonth = getFirstDayOfMonth(date);
+        System.out.println(firstDayOfMonth);
+        System.out.println("getLastDayOfMonth : " + getLastDayOfMonth(date));
+    }
+
+    public static int getMonth(Date date){
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        return calendar.get(Calendar.MONTH);
+    }
+    public static int getYear(Date date){
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        return calendar.get(Calendar.YEAR);
+    }
+
+    public static Date getFirstDayOfMonth(Date date){
+        Calendar instance = Calendar.getInstance();
+        instance.set(Calendar.YEAR,getYear(date));
+        instance.set(Calendar.MONTH,getMonth(date));
+        int actualMinimum = instance.getActualMinimum(Calendar.DAY_OF_MONTH);
+        instance.set(Calendar.DAY_OF_MONTH,actualMinimum);
+        return instance.getTime();
+    }
+
+    public static Date getLastDayOfMonth(Date date){
+        Calendar instance = Calendar.getInstance();
+        instance.set(Calendar.YEAR,getYear(date));
+        instance.set(Calendar.MONTH,getMonth(date));
+        int actualMinimum = instance.getActualMaximum(Calendar.DAY_OF_MONTH);
+        instance.set(Calendar.DAY_OF_MONTH,actualMinimum);
+        return instance.getTime();
     }
 }
