1498a35614e742d3099ade5ef7b39ab12f47c00c
yanzhitao <yanzhitao@xiaomalixing.com>
2019-06-20 20:59:10 +0800

    feature:mybatis自定义plugin 记录sql执行时间

---
+++ springboot-mybatis/src/main/java/com/example/yyw/mybatis/config/SqlCostInterceptor.java
@@ -0,0 +1,44 @@
+package com.example.yyw.mybatis.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.executor.statement.StatementHandler;
+import org.apache.ibatis.plugin.*;
+import org.apache.ibatis.session.ResultHandler;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+import java.sql.Statement;
+import java.util.Properties;
+
+/**
+ * @author yanzhitao@xiaomalixing.com
+ * @date 2019/6/20 20:13
+ * @describe Sql执行时间记录拦截器
+ */
+@Intercepts({@Signature(type = StatementHandler.class, method = "query", args = {Statement.class, ResultHandler.class}),
+        @Signature(type = StatementHandler.class, method = "update", args = {Statement.class}),
+        @Signature(type = StatementHandler.class, method = "batch", args = { Statement.class })})
+@Slf4j
+@Component
+public class SqlCostInterceptor implements Interceptor {
+    @Override
+    public Object intercept(Invocation invocation) throws Throwable {
+        long start = System.currentTimeMillis();
+        try {
+            return invocation.proceed();
+        } finally {
+            long endTime = System.currentTimeMillis();
+            log.info("执行耗时: [{}] ms", endTime - start);
+        }
+    }
+
+    @Override
+    public Object plugin(Object target) {
+        return Plugin.wrap(target, this);
+    }
+
+    @Override
+    public void setProperties(Properties properties) {
+
+    }
+}
