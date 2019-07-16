42953751d8b2304dd58b1e3e2870a084541af326
yanzhitao <yanzhitao@xiaomalixing.com>
2019-07-16 19:20:44 +0800

    feature: 整合es，x-pack认证连接(注意elasticsearch版本相对应)

--- springboot-elasticsearch/src/main/java/com/example/springbootelasticsearch/config/ElasticsearchConfig.java
+++ springboot-elasticsearch/src/main/java/com/example/springbootelasticsearch/config/ElasticsearchConfig.java
@@ -2,7 +2,7 @@ package com.example.springbootelasticsearch.config;
 
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.common.transport.TransportAddress;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
 import org.elasticsearch.xpack.client.PreBuiltXPackTransportClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -19,13 +19,29 @@ import java.net.UnknownHostException;
 @Configuration
 public class ElasticsearchConfig {
 
+    @Value("${spring.data.elasticsearch.username:elastic}")
+    private String username;
+
+    @Value("${spring.data.elasticsearch.password:changeme}")
+    private String password;
+
+    @Value("${spring.data.elasticsearch.cluster-name:elasticsearch}")
+    private String clusterName;
+
+    @Value("${spring.data.elasticsearch.cluster-nodes:192.168.222.128:9300}")
+    private String clusterNodes;
+
+    private static final String SPLIT_STR = ":";
+
     @Bean
     public TransportClient transportClient() throws UnknownHostException {
+        String[] split = clusterNodes.split(SPLIT_STR);
         TransportClient client = new PreBuiltXPackTransportClient(Settings.builder()
-                .put("cluster.name", "elasticsearch")
-                .put("xpack.security.user", "elastic:changeme")
+                .put("cluster.name", clusterName)
+                .put("xpack.security.user", username + SPLIT_STR + password)
                 .build())
-                .addTransportAddress(new TransportAddress(InetAddress.getByName("127.0.0.1"), 9300));
+                .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName(split[0]), Integer.valueOf(split[1])));
         return client;
     }
+
 }
