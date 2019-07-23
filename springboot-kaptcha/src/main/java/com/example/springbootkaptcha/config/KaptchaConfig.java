0ab0efa8959c1386c1e2023d7a909c631ffcb58d
yanzhitao <yanzhitao@xiaomalixing.com>
2019-07-23 18:12:45 +0800

    feature: 图形验证码

--- springboot-kaptcha/src/main/java/com/example/springbootkaptcha/config/KaptchaConfig.java
+++ springboot-kaptcha/src/main/java/com/example/springbootkaptcha/config/KaptchaConfig.java
@@ -20,25 +20,48 @@ public class KaptchaConfig {
     public DefaultKaptcha defaultKaptcha(){
         DefaultKaptcha kaptcha = new DefaultKaptcha();
         Properties properties=new Properties();
-        properties.setProperty(Constants.KAPTCHA_IMAGE_WIDTH, "125");
-        properties.setProperty(Constants.KAPTCHA_IMAGE_HEIGHT, "45");
-        properties.setProperty(Constants.KAPTCHA_TEXTPRODUCER_FONT_SIZE, "22");
-        properties.setProperty(Constants.KAPTCHA_TEXTPRODUCER_FONT_COLOR, "black");
-        properties.setProperty(Constants.KAPTCHA_TEXTPRODUCER_CHAR_LENGTH, "6");
-        properties.setProperty(Constants.KAPTCHA_TEXTPRODUCER_CHAR_SPACE, "6");
+        //是否有边框 默认为true 我们可以自己设置yes，no
+        properties.setProperty(Constants.KAPTCHA_BORDER, "no");
+        //边框颜色 默认为Color.BLACK
 //        properties.setProperty(Constants.KAPTCHA_BORDER_COLOR, "105,179,90");
         properties.setProperty(Constants.KAPTCHA_BORDER_COLOR, "LIGHT_GRAY");
-        properties.setProperty(Constants.KAPTCHA_BACKGROUND_CLR_FROM, "WHITE");
-        //验证码的干扰线
+        //边框粗细度 默认为1
+        properties.setProperty(Constants.KAPTCHA_BORDER_THICKNESS, "1");;
+        // 验证码噪点颜色 默认为Color.BLACK
+        properties.setProperty(Constants.KAPTCHA_NOISE_COLOR, "BLACK");
+        //验证码噪点生成对象 默认为DefaultNoise
         properties.setProperty(Constants.KAPTCHA_NOISE_IMPL, "com.google.code.kaptcha.impl.DefaultNoise");
-        //验证码的样式
+        //验证码样式引擎 默认为WaterRipple
         properties.setProperty(Constants.KAPTCHA_OBSCURIFICATOR_IMPL, "com.google.code.kaptcha.impl.ShadowGimpy");
+        //验证码生成器 默认为DefaultKaptcha
+        properties.setProperty(Constants.KAPTCHA_PRODUCER_IMPL, "com.google.code.kaptcha.impl.DefaultKaptcha");
+        //验证码文本生成器 默认为DefaultTextCreator
+        properties.setProperty(Constants.KAPTCHA_TEXTPRODUCER_IMPL, "com.google.code.kaptcha.text.impl.DefaultTextCreator");
+        // 验证码文本字符内容范围 默认为abcde2345678gfynmnpwx
         properties.setProperty(Constants.KAPTCHA_TEXTPRODUCER_CHAR_STRING, "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
+        // 验证码文本字符长度 默认为5
+        properties.setProperty(Constants.KAPTCHA_TEXTPRODUCER_CHAR_LENGTH, "6");
+        // 验证码文本字体样式 默认为new Font("Arial", 1, fontSize), new Font("Courier", 1, fontSize)
+//        properties.setProperty(Constants.KAPTCHA_TEXTPRODUCER_FONT_NAMES, "125");
+        //验证码文本字符颜色 默认为Color.BLACK
+        properties.setProperty(Constants.KAPTCHA_TEXTPRODUCER_FONT_COLOR, "black");
+        //验证码文本字符大小 默认为40
+        properties.setProperty(Constants.KAPTCHA_TEXTPRODUCER_FONT_SIZE, "22");
+        //验证码文本字符间距 默认为2
+        properties.setProperty(Constants.KAPTCHA_TEXTPRODUCER_CHAR_SPACE, "6");
+        //验证码文本字符渲染 默认为DefaultWordRenderer
+        properties.setProperty(Constants.KAPTCHA_WORDRENDERER_IMPL, "com.google.code.kaptcha.text.impl.DefaultWordRenderer");
+        //验证码背景生成器 默认为DefaultBackground
+        properties.setProperty(Constants.KAPTCHA_BACKGROUND_IMPL, "com.google.code.kaptcha.impl.DefaultBackground");
+        //验证码背景颜色渐进 默认为Color.LIGHT_GRAY
+        properties.setProperty(Constants.KAPTCHA_BACKGROUND_CLR_FROM, "WHITE");
+        //验证码背景颜色渐进 默认为Color.WHITE
+        properties.setProperty(Constants.KAPTCHA_BACKGROUND_CLR_TO, "WHITE");
+        //验证码图片宽度 默认为200
+        properties.setProperty(Constants.KAPTCHA_IMAGE_WIDTH, "125");
+        //验证码图片高度 默认为50
+        properties.setProperty(Constants.KAPTCHA_IMAGE_HEIGHT, "45");
 
-
-//        properties.setProperty(Constants.KAPTCHA_BORDER, "yes");
-//        properties.setProperty(Constants.KAPTCHA_TEXTPRODUCER_FONT_NAMES, "宋体,楷体,微软雅黑");
-//        properties.setProperty(Constants.KAPTCHA_SESSION_KEY, "code");
         Config config=new Config(properties);
         kaptcha.setConfig(config);
         return kaptcha;
