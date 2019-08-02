e9413e8c66ac4a5491cbb57e74a004c5d23e35dd
yanzhitao <yanzhitao@xiaomalixing.com>
2019-08-02 19:00:02 +0800

    feature: 经纬度计算工具类

---
+++ springboot-example-common/src/main/java/com/example/yyw/util/CaculateDistanceUtil.java
@@ -0,0 +1,44 @@
+package com.example.yyw.util;
+
+import org.gavaghan.geodesy.Ellipsoid;
+import org.gavaghan.geodesy.GeodeticCalculator;
+import org.gavaghan.geodesy.GeodeticCurve;
+import org.gavaghan.geodesy.GlobalCoordinates;
+
+/**
+ * @author yanzhitao@xiaomalixing.com
+ * @date 2019/8/2 18:07
+ * @describe
+ */
+public class CaculateDistanceUtil {
+
+    public static void main(String[] args)
+    {
+        GlobalCoordinates source = new GlobalCoordinates(29.490295, 106.486654);
+        GlobalCoordinates target = new GlobalCoordinates(29.615467, 106.581515);
+
+        double meter1 = getDistanceMeter(source, target, Ellipsoid.Sphere);
+        double meter2 = getDistanceMeter(source, target, Ellipsoid.WGS84);
+
+        System.out.println("Sphere坐标系计算结果："+meter1 + "米");
+        System.out.println("WGS84坐标系计算结果："+meter2 + "米");
+
+        // 112.76535,22.75424  113.10111,23.014171
+        source = new GlobalCoordinates(22.75424, 112.76535);
+        target = new GlobalCoordinates(23.014171, 113.10111);
+
+        meter1 = getDistanceMeter(source, target, Ellipsoid.Sphere);
+        meter2 = getDistanceMeter(source, target, Ellipsoid.WGS84);
+
+        System.out.println("Sphere坐标系计算结果："+meter1 + "米");
+        System.out.println("WGS84坐标系计算结果："+meter2 + "米");
+    }
+
+    public static double getDistanceMeter(GlobalCoordinates gpsFrom, GlobalCoordinates gpsTo, Ellipsoid ellipsoid)
+    {
+        //创建GeodeticCalculator，调用计算方法，传入坐标系、经纬度用于计算距离
+        GeodeticCurve geoCurve = new GeodeticCalculator().calculateGeodeticCurve(ellipsoid, gpsFrom, gpsTo);
+
+        return geoCurve.getEllipsoidalDistance();
+    }
+}
