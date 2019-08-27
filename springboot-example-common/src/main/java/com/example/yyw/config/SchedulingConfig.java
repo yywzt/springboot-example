90d9dfdc04adba8bbf0af439cf204566c530d15f
yanzhitao <yanzhitao@xiaomalixing.com>
2019-08-27 16:58:37 +0800

    freature: 增加异步执行线程池(@Async)以及定时任务线程池(Schedule)

--- springboot-example-common/src/main/java/com/example/yyw/config/SchedulingConfig.java
+++ springboot-example-common/src/main/java/com/example/yyw/config/SchedulingConfig.java
@@ -16,6 +16,10 @@ public class SchedulingConfig {
 
     /**当前线程池数*/
     private int poolSize = 10;
+    /**等待任务在关机时完成--true表明等待所有线程执行完成再优雅停机*/
+    private boolean waitForTasksToCompleteOnShutdown = true;
+    /**等待时间 （默认为0，此时立即停止），并等待xx秒后强制停止*/
+    private int awaitTerminationSeconds = 30;
     /**线程名称前缀*/
     private String threadNamePrefix = "MyScheduler-";
 
@@ -23,6 +27,8 @@ public class SchedulingConfig {
     public TaskScheduler taskScheduler(){
         ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler();
         taskScheduler.setPoolSize(poolSize);
+        taskScheduler.setWaitForTasksToCompleteOnShutdown(waitForTasksToCompleteOnShutdown);
+        taskScheduler.setAwaitTerminationSeconds(awaitTerminationSeconds);
         taskScheduler.setThreadNamePrefix(threadNamePrefix);
         taskScheduler.initialize();
         return taskScheduler;
